b1db52fd114c4af62ae2f592c1a8f26c006f9b41
Maxim Papko <papkom50@gmail.com>
2024-12-22 01:19:56 +0200

    add a new blueprint registration

--- applab2.py
+++ applab2.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 
 # Реєструємо "блюпринти" для кожної групи ендпоінтів
 app.register_blueprint(users_bp, url_prefix='/api')
-app.register_blueprint(categories_bp, url_prefix='/api')
-app.register_blueprint(records_bp, url_prefix='/api')
+'''app.register_blueprint(categories_bp, url_prefix='/api')
+app.register_blueprint(records_bp, url_prefix='/api')'''
 
 if __name__ == '__main__':
     app.run(debug=True)
