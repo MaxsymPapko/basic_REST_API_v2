c6a7b3ca46d20ccc0666b6428c950f6ac3555156
Maxim Papko <papkom50@gmail.com>
2024-12-22 01:23:58 +0200

    fixed a flask-usage name

--- applab2.py
+++ applab2.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import flask
 from routes.users import users_bp
 from routes.categories import categories_bp
 from routes.records import records_bp
 
-app = Flask(__name__)
+app = flask(__name__)
 
 # Реєструємо "блюпринти" для кожної групи ендпоінтів
 app.register_blueprint(users_bp, url_prefix='/api')
