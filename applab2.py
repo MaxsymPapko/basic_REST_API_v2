8a6cd5ccaed0e5d4dbd38b1e037941ca0c1223c3
Maxim Papko <papkom50@gmail.com>
2025-01-01 23:38:28 +0200

    add a root route to applab2 file; edited db url in config file

--- applab2.py
+++ applab2.py
@@ -1,8 +1,7 @@
 from flask import Flask
+from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
-from config import Config
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -11,14 +10,25 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    # Ініціалізація db та migrate після створення app
     db.init_app(app)
     migrate.init_app(app, db)
 
+    # Тепер імпортуємо blueprint без циклічних імпортів
     from routes.users import users_bp
+    from routes.currencies import currencies_bp
+
+    # Реєстрація маршрутів
     app.register_blueprint(users_bp, url_prefix='/api')
+    app.register_blueprint(currencies_bp, url_prefix='/api')
+
+    @app.route('/')
+    def home():
+        return 'Hello, World!'
 
     return app
 
 if __name__ == '__main__':
     app = create_app()
     app.run(debug=True)
+
